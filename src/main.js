17e4401d67267699a9019096e520948927086dfd
Asparuh Nestorov <anestorov@gmail.com>
2021-08-29 01:15:49 +0300

    apollo added

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,65 @@
 <template>
-  <hello-world />
+    <v-container>
+        <v-card>
+            <v-card-text>
+                <ul>
+                    <li v-for="(post,k) in postsCreated" :key="k">{{post.author}} / {{post.comment}}</li>
+                </ul>
+            </v-card-text>
+            <v-card-actions>
+                <v-row>
+                    <v-col>
+                        <v-text-field label="author" v-model="author"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field label="comment" v-model="comment"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-btn @click="send()">SEND</v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-actions>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import MM from "../graphql/MyMutation.gql"
+import MyS from "../graphql/MyS.gql";
+export default {
+    name: "Home",
+    data() {
+        return {
+            books: [],
+            postsCreated: [],
+            author:null,
+            comment:null,
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
+        };
     },
-  }
+    methods: {
+        send() {
+            this.$apollo.mutate({
+                mutation:MM,
+                variables: {
+                    makePostAuthor : this.author,
+                    makePostComment : this.comment,
+                }
+            })
+        }
+    },
+    apollo: {
+        //books: {
+        //query: MyQ,
+        //},
+        $subscribe: {
+            postCreated: {
+                query: MyS,
+                result({ data }) {
+                    this.postsCreated.push(data.postCreated);
+                },
+            },
+        },
+    },
+};
 </script>
